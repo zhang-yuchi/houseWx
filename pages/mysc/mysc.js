4d558bb25b06433c4b9c2c2ec417a662f3b546fe
wangtanke <1665497618@qq.com>
2019-12-28 16:24:00 +0800

    mdf

--- pages/mysc/mysc.js
+++ pages/mysc/mysc.js
@@ -1,6 +1,7 @@
 // pages/myorder/myorder.js
 var app = getApp();
 var ajax = require('../../utils/ajax.js')
+var tagsToArr = require('../../utils/utils.js')
 var allArr = [];
 Page({
 
@@ -9,87 +10,127 @@ Page({
    */
   data: {
     scrollViewHeight: "",
-    barArr: [
-      { name: "房源信息", id: 0, className: "inner-btn select-btn" },
-      { name: "生活服务", id: 1, className: "inner-btn" }
+    barArr: [{
+        name: "房源信息",
+        id: 0,
+        className: "inner-btn select-btn",
+        select: true
+      },
+      {
+        name: "生活服务",
+        id: 1,
+        className: "inner-btn",
+        select: false
+      }
     ],
     houseSets: [],
+    house: [],
+    store: [],
     host: app.data.requestHost
   },
-  tohousedetails(e){
+  tohousedetails(e) {
     let index = e.currentTarget.dataset.id
     wx.navigateTo({
-      url: '../housedetail/housedetail?obj='+index,
+      url: '../housedetail/housedetail?obj=' + index,
     })
   },
-  changemodel(e){
+  changemodel(e) {
+    var that = this;
     let index = e.currentTarget.dataset.index
     console.log(index)
-    for(let i in this.data.barArr){
-      if(i==index){
+    for (let i in this.data.barArr) {
+      if (i == index) {
         this.data.barArr[i].className = "inner-btn select-btn"
-      }else{
+        this.data.barArr[i].select = true;
+      } else {
         this.data.barArr[i].className = "inner-btn"
+        this.data.barArr[i].select = false;
       }
     }
-    for(let item of this.data.barArr){
-      if(item.className == "inner-btn select-btn"){
-        if(item.name == "房源信息"){
+    for (let item of this.data.barArr) {
+      if (item.className == "inner-btn select-btn") {
+        if (item.name == "房源信息") {
           //此时请求房源接口
-
-        }else{
+          ajax.requestByGet('/user/star/house', {}, function(res) {
+            console.log(res)
+            let list = res.data.data
+            for(let i=0;i<list.length;i++){
+              list[i].tags = tagsToArr.tagsToArr(list[i].tags);
+            }
+            that.setData({
+              house: list
+            })
+          })
+        } else {
           //此时请求生活服务接口
-
+          ajax.requestByGet('/user/star/store', {}, function(res) {
+            console.log(res)
+            let list = res.data.data
+            for (let i = 0; i < list.length; i++) {
+              list[i].tags = tagsToArr.tagsToArr(list[i].tags);
+            }
+            that.setData({
+              store: list
+            })
+          })
         }
       }
     }
     this.setData({
-      barArr:this.data.barArr
+      barArr: this.data.barArr
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    ajax.requestByGet('/user/star/house',{},function(res){
+  onLoad: function(options) {
+    let that = this;
+    ajax.requestByGet('/user/star/house', {}, function(res) {
       console.log(res)
-    })
-  },
-  changeBar: function (e) {
-    var that = this;
-    var id = e.currentTarget.id;
-    var arr = that.data.barArr;
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].id == id) {
-        arr[i].className = "son_text"
-      } else {
-        arr[i].className = "son_textC"
+      let list = res.data.data
+      for (let i = 0; i < list.length; i++) {
+        list[i].tags = tagsToArr.tagsToArr(list[i].tags);
       }
-    }
-    that.setData({
-      barArr: arr
+      that.setData({
+        house:list
+      })
     })
   },
+  // changeBar: function (e) {
+  //   var that = this;
+  //   var id = e.currentTarget.id;
+  //   var arr = that.data.barArr;
+  //   for (var i = 0; i < arr.length; i++) {
+  //     if (arr[i].id == id) {
+  //       arr[i].className = "son_text"
+  //     } else {
+  //       arr[i].className = "son_textC"
+  //     }
+  //   }
+  //   that.setData({
+  //     barArr: arr
+  //   })
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     var that = this;
     var id = wx.getStorageSync('id');
     that.setData({
       scrollViewHeight: (app.data.height - 50) + "px"
     });
-    
+
   },
-  tohousedetail: function (e) {
+  tohousedetail: function(e) {
     var that = this;
     var id = e.currentTarget.id;
     var arr = that.data.houseSets;
@@ -108,35 +149,35 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
